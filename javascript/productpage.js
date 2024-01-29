90800f470b719228e1a4d4be62a6e757fc1a9d34
adigic <adigic@hotmail.com>
2024-01-29 10:25:47 +0100

    Tagit bort lite element för att underlätta arbetet, fixat till lite i css. Samt ändrat hur priset visas i javascript.

--- javascript/productpage.js
+++ javascript/productpage.js
@@ -23,7 +23,7 @@ function updateProductPage(type, color, image, id, price) {
   productNameElement.textContent = `${color} ${type}`;
 
   const productPriceElement = document.getElementById("productPrice");
-  productPriceElement.textContent = `${price}kr`;
+  productPriceElement.textContent = `${price}:-`;
 
   // Update product image in the carousel
   const carouselInner = document.querySelector(".carousel-inner");
